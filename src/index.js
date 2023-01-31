8218134b883d0cc11e179d76a763dd9868e5f271
Samuel <samuel.estudo778@gmail.com>
2023-01-31 13:49:55 -0300

    chore: install eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world, initial settings')
-})
+  res.send('hello world, initial settings');
+});
 
-app.listen(3333, () => console.log('Server started at http://localhost:3333'))
+app.listen(3333, () => console.log('Server started at http://localhost:3333'));
