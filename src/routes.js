e4a440c4e98ff662f7d20b0ad4feb696550ea341
Samuel <samuel.estudo778@gmail.com>
2023-02-08 17:20:53 -0300

    feat: create CategoryController and CategoriesRepository

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
+// contacts
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
 router.delete('/contacts/:id', ContactController.delete);
+
+// categories
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
